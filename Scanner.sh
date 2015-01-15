a42f8aa30562cd6e327388303c8bc4155f9de01d
juanvallejo <juan.vallejo.12@cnu.edu>
2015-01-15 11:09:32 -0500

    modify exec path

--- Scanner.sh
+++ Scanner.sh
@@ -9,22 +9,24 @@ PLATFORM="osx";
 UNAME_OUT=`uname`;
 
 # define app variables
-PATH_TO_EXEC="./js/scanner.js";
+PATH_TO_EXEC="js/scanner.js";
 APP_UI_ADDRESS="http://localhost:8000";
 
 if [[ "$UNAME_OUT" == 'Linux' ]]; then
 
    PLATFORM='linux';
+   ROOT_PATH='/home/pcse/Documents/pcse-scanner';
    google-chrome $APP_UI_ADDRESS;
 
 elif [[ "$UNAME_OUT" == 'Darwin' ]]; then
 
    PLATFORM='osx';
+   ROOT_PATH="./";
    open -a "Google Chrome" $APP_UI_ADDRESS;
 
 fi
 
 
 wait;
-node $PATH_TO_EXEC;
+node $ROOT_PATH$PATH_TO_EXEC;
 
